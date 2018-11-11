8a46292fad6efce1db5e7a72f3b6f579621a725e
Ilona Sellenberkova <ilona.sell@gmail.com>
2018-11-11 15:54:31 +0100

    starting React app App.css, logo deleted

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My First ract app!</h1>
+        <p>Welcome ;)</p>
+      </div>
+    );
+  }
+}
+
+export default App;
