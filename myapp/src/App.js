d9430c4efbd891dfef434cb5d4d59ad71089f1d3
Ilona Sellenberkova <ilona.sell@gmail.com>
2018-11-11 16:50:34 +0100

    Props passing to child component Ninjas.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
         <h1>My First React app!</h1>
         <p>Welcome ;)</p>
         {/* nesting component Ninjas */}
-        <Ninjas />
+        {/* creating props and passing to child component Ninjas.js */}
+        <Ninjas name="Ryu" age="30" belt="black"/>
+        <Ninjas name="Yoshi" age="28" belt="green"/>
       </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 // now we import React so its not React.create.Component...
+// promps pass data from parent component to child component(app.js to Ninjas.js)
 class Ninjas extends Component {
     render() {
+        // props inside render method
+        // {name: "Ryu", age: "30", belt: "black"}
+        // console.log(this.props);
+        const { name, age, belt} = this.props;
+        // this.props.name putting to div instead
         return(
             <div className="ninja">
-            <div>Name: Ryu</div>
-            <div>Age: 30</div>
-            <div>Belt: Black</div>
+            <div>Name: { name }</div>
+            <div>Age: { age }</div>
+            <div>Belt: { belt }</div>
         </div>
         )
     }
